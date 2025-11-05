ef0bff6e2745750c932cfa7f9529db5b343557af
Ryystuffs <131592912+Ryystuffs@users.noreply.github.com>
2025-11-05 21:31:56 +0800

    feat: fixed design in landing page,

--- resources/views/member/memberAccount.blade.php
+++ resources/views/member/memberAccount.blade.php
@@ -1,16 +1,16 @@
-<x-membernav >
+<x-membernav>
 
     <div class="flex flex-col h-screen justify-center items-center ">
-        <div class="text-black flex flex-col justify-center items-center p-9 bg-white rounded-4xl border-2 border-gray-600 w-4/5 min-h-[600px]">   
+        <div class="text-black flex flex-col justify-center items-center p-9 bg-[#f3f3f3] rounded-4xl border-2 border-gray-600 w-4/5 min-h-[600px]">
             <div class="flex flex-col justify-center items-center text-center">
                 <span class="text-3xl font-bold">Qr Code:</span>
                 <img src="{{ asset('storage/' . $user->qrcode) }}" alt="" class="my-5 w-[200px] h-[200px]">
             </div>
             <div class="mt-4">
-                <h3>Direction of use:</h3>
-                <p>1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eos</p>
-                <p> 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti eos ipsum quo sapiente maxime,</p>
-                <p>3. corrupti deserunt incidunt et, veniam at est, assumenda dignissimos architecto dolorum iste tempore qui eius fugit! Voluptatibus, sit!</p>
+                <h3 class="text-2xl mb-1.5">Direction of Use:</h3>
+                <p>1. Scan the QR code at the gym entrance scanner to check in. </p>
+                <p>2. Wait for the confirmation message or blue light before entering.</p>
+                <p>3. When leaving the gym, scan the same QR code again at the exit scanner to record your check-out time.</p>
             </div>
         </div>
 
